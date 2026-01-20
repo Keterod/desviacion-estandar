214a28f3486be67624ea257c5a4c74248c0a1706
DCarhuamaca <71624462@continental.edu.pe>
2026-01-19 19:13:56 -0500

    Caso 2: Calcular desviacion con un conjunto de un elemento (Rojo)

--- tests/Testdatos.py
+++ tests/Testdatos.py
@@ -6,3 +6,7 @@ class TestDatos(unittest.TestCase):
     def test_conjunto_vacio_retornaNone(self):
         datos = Datos([])
         self.assertIsNone(Datos.desviacion(self))
+
+    def test_conjunto_unElemento_retornaValorZERO(self):
+        datos = Datos([5])
+        self.assertEqual(0, datos.desviacion())
