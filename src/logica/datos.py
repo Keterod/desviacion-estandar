b433e19339dc43607822e6436e80c624475d6494
DCarhuamaca <71624462@continental.edu.pe>
2026-01-19 19:38:50 -0500

    Caso 3: Calcular desviacion con un conjunto de N elemento (Azul)

--- src/logica/datos.py
+++ src/logica/datos.py
@@ -3,9 +3,7 @@ class Datos:
         self.__datos = datos
 
     def desviacion(self):
-        if len(self.__datos) == 1:
-            return 0
-        if len(self.__datos) > 1:
+        if len(self.__datos) >= 1:
             media = sum(self.__datos) / len(self.__datos)
             suma_cuadrados = sum((x - media) ** 2 for x in self.__datos)
             desviacion = (suma_cuadrados / len(self.__datos)) ** 0.5
--- tests/Testdatos.py
+++ tests/Testdatos.py
@@ -10,9 +10,11 @@ class TestDatos(unittest.TestCase):
     def test_conjunto_unElemento_retornaValorZERO(self):
         datos = Datos([5])
         self.assertEqual(0, datos.desviacion())
-    def test_conjunto_nElementos_retornaPromedioNElementos( self ):
-        datos=Datos([2,4,6,8])
-        media=(2+4+6+8)/4
-        suma_cuad=(((2-media)**2)+((4-media)**2)+((6-media)**2)+((8-media)**2))
-        desvi= (suma_cuad/4) **0.5
-        self.assertEqual(desvi,datos.desviacion())
+
+    def test_conjunto_nElementos_retornaPromedioNElementos(self):
+        datos = Datos([2, 4, 6, 8])
+        media = (2+4+6+8)/4
+        suma_cuad = (((2-media)**2)+((4-media)**2) +
+                     ((6-media)**2)+((8-media)**2))
+        desvi = (suma_cuad/4) ** 0.5
+        self.assertEqual(desvi, datos.desviacion())
