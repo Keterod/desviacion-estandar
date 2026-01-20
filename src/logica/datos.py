a26b26604bb08f3d77ea24693a95bc9ec77b17aa
DCarhuamaca <71624462@continental.edu.pe>
2026-01-19 19:35:09 -0500

    Caso 3: Calcular desviacion con un conjunto de N elemento (Verde)

--- src/logica/datos.py
+++ src/logica/datos.py
@@ -5,6 +5,10 @@ class Datos:
     def desviacion(self):
         if len(self.__datos) == 1:
             return 0
+        if len(self.__datos) > 1:
+            media = sum(self.__datos) / len(self.__datos)
+            suma_cuadrados = sum((x - media) ** 2 for x in self.__datos)
+            desviacion = (suma_cuadrados / len(self.__datos)) ** 0.5
+            return desviacion
         else:
             return None
-
